4fe7e4679e7c115df0538652c48e70d90f09b3e6
Mark Stam <markstam099@gmail.com>
2023-04-22 20:11:00 +0200

    Bootstrap and error handling

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const talkBtn = document.querySelector("#praat");
 const talkInput = document.querySelector("#talkInput");
-
+let alertBox;
+let alertClone;
 talkBtn.addEventListener('click', playAudios);
 talkInput.addEventListener('keyup', (event) => {
     event.preventDefault();
@@ -13,6 +14,8 @@ talkInput.addEventListener('keyup', (event) => {
 let queue = [];
 
 function playAudios() {
+    talkInput.classList.remove("is-invalid");
+
     // Fill queue with words
     let words = talkInput.value.split(" ");
     for (let word of words) {
@@ -31,16 +34,32 @@ function playNext() {
 
 function playSound(word, retry = true) {
     let audio = new Audio(`https://commons.wikimedia.org/wiki/Special:FilePath/nl-${word}.ogg`);
-    audio.play().catch((error) => {
+    audio.play().catch(() => {
         if(retry) {
             console.log("Retry...");
             // Make first letter uppercase and retry
             word = word.charAt(0).toUpperCase() + word.slice(1);
             // We stop after one retry
             playSound(word, false);
+        } else {
+            talkInput.classList.add("is-invalid");
+            alertBox = document.querySelector("#alert");
+            alertClone = alertBox.cloneNode(true);
+            document.querySelector("#alert-container").appendChild(alertClone);
+
+            // set text
+            alertBox.children[0].textContent = word.toLowerCase();
+
+            alertBox.classList.remove("d-none");
+            alertBox.classList.add("show");
+            playNext();
         }
     });
-
     // Play next audio
     audio.onended = playNext;
 }
+
+// Replace the alert with the clone
+$('#alert').on('close.bs.alert', function () {
+    alertBox = alertClone;
+})
